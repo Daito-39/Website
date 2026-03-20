2b50b741c10ac3abfb605e9e0f386eec9a2193ac
Daito-39 <ellismckeown@yahoo.com>
2026-03-20 14:35:47 +0000

    ability to minimise, return and eradicate panels with the menu options

--- public/togglePanel.js
+++ public/togglePanel.js
@@ -1,6 +1,7 @@
 const panel1 = document.getElementById("panel1");
 const panel2 = document.getElementById("panel2");
 const panel3 = document.getElementById("panel3");
+const panel4 = document.getElementById("panel4");
 
 const panel1Media = document.getElementById("panel1Media");
 const panel2Media = document.getElementById("panel2Media");
@@ -9,34 +10,42 @@ const panel4Media = document.getElementById("panel4Media");
 
 function minimise_panel(panelNo) {
     if (panelNo == 1) {
-        if (panel1Media.style.display == 'none') {
-            panel1Media.style.display = 'initial';
-            panel1.style.paddingBottom = '10em';
-        } else {
-            panel1Media.style.display = 'none';
-            panel1.style.paddingBottom = '0em';
-        }
+        panel1Media.style.display = 'none';
+        panel1.style.paddingBottom = '0em';
     } else if (panelNo == 2) {
-        if (panel2Media.style.display == 'none') {
-            panel2Media.style.display = 'initial';
-            panel2.style.paddingBottom = '0.5em';
-        } else {
-            panel2Media.style.display = 'none';
-            panel2.style.paddingBottom = '0em';
-        }
+        panel2Media.style.display = 'none';
+        panel2.style.paddingBottom = '0em';
     } else if (panelNo == 3) {
-        if (panel3Media.style.display == 'none') {
-            panel3Media.style.display = 'initial';
-            panel3.style.paddingBottom = "10em";
-        } else {
-            panel3Media.style.display = 'none';
-            panel3.style.paddingBottom = "0em";
-        }
+        panel3Media.style.display = 'none';
+        panel3.style.paddingBottom = "0em";
     } else if (panelNo == 4) {
-        if (panel4Media.style.display == 'none') {
-            panel4Media.style.display = 'flex';
-        } else {
-            panel4Media.style.display = 'none';
-        }
+        panel4Media.style.display = 'none';
+    }
+}
+
+function return_panel(panelNo) {
+    if (panelNo == 1) {
+        panel1Media.style.display = 'initial';
+        panel1.style.paddingBottom = '10em';
+    } else if (panelNo == 2) {
+        panel2Media.style.display = 'initial';
+        panel2.style.paddingBottom = '0.5em';
+    } else if (panelNo == 3) {
+        panel3Media.style.display = 'initial';
+        panel3.style.paddingBottom = "10em";
+    } else if (panelNo == 4) {
+        panel4Media.style.display = 'flex';
+    }
+}
+
+function eradicate_panel(panelNo) {
+    if (panelNo == 1) {
+        panel1.style.display = 'none';
+    } else if (panelNo == 2) {
+        panel2.style.display = 'none';
+    } else if (panelNo == 3) {
+        panel3.style.display = 'none';
+    } else if (panelNo == 4) {
+        panel4.style.display = 'none';
     }
 }
